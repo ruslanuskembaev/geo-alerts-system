5b8c27e647be8e5927deb93d86645023f4391a09
Руслан Ускембаев <ruslanuskembaev02@gmail.com>
2026-01-08 08:17:51 +0700

    Add Gin framework and basic HTTP server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Geo Alerts System starting...")
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run(":8080")
 }
