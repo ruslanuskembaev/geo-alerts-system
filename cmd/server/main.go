07761afcd81a05482317c012c895080b65fa2dee
Руслан Ускембаев <ruslanuskembaev02@gmail.com>
2026-01-10 08:35:45 +0700

    Add .env file support and environment variable loading

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,15 +4,28 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/ruslanuskembaev/geo-alerts-system/internal/config"
 )
 
 func main() {
+
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("No .env file found, proceeding with environment variables")
+	}
+
 	cfg := config.Load()
-
 	fmt.Printf("Starting server on port %s\n", cfg.ServerPort)
-
 	r := gin.Default()
+
+	r.GET("/secure", func(c *gin.Context) {
+		apiKey := c.GetHeader("X-API-KEY")
+		if apiKey != cfg.APIKey {
+			c.JSON(401, gin.H{"error": "Unauthorized"})
+			return
+		}
+		c.JSON(200, gin.H{"message": "Authorized access"})
+	})
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
